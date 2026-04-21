82069f414d0be56a9c6b240b7c50a54bd89bdc22
jenwils <jen.wils98@gmail.com>
2026-04-20 20:18:11 -0400

    feat: Checks if user is logged in.
    If user is not logged in, they are sent to the login page.

--- src/main/java/DeleteDeliverableServlet.java
+++ src/main/java/DeleteDeliverableServlet.java
@@ -2,15 +2,19 @@ import java.io.IOException;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.*;
 
 @WebServlet("/deleteTask")
 public class DeleteDeliverableServlet extends HttpServlet{
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+		
+		HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute("studentID") == null) {
+            response.sendRedirect("Login_Register.html");
+            return;
+        }
 		
 		int deliverableId = Integer.parseInt(request.getParameter("id"));
 		
