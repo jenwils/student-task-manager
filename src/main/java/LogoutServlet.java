509d0ec3e358e3fedf4e0a35d307aedf6832cd2f
jenwils <jen.wils98@gmail.com>
2026-04-20 22:57:03 -0400

    feat: Added Servlet to log out user.

---
+++ src/main/java/LogoutServlet.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+ 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+ 
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+ 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+ 
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+ 
+        response.sendRedirect("Login_Register.html");
+    }
+}
+ 
